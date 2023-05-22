3aa968a1ff18ea90404e01a1b4d4d0b79467d3d3
Mohammed Shousha <mshousha29@gmail.com>
2023-05-22 18:06:57 +0300

    updating gpio

--- gpio.py
+++ gpio.py
@@ -1,4 +1,4 @@
-from servo import servo_angle, mid_servo, min_servo, max_servo
+from servo import servo1, servo2
 from stepper import step_forward, step_backward
 from time import sleep
 
@@ -11,17 +11,44 @@ from time import sleep
 # move back x-axis stepper
 # move back servo
 
-DEGREES = 360
+DEGREES = 180
+
+servos = [servo1, servo2]
 
 
 def get_medicine(position):
-    print(position)
+    row, col = position.values()
+    print(f"row: {row}, col: {col}")
+
+    servo1.min()
+    sleep(1)
+
+    step_forward(DEGREES * row)
+    sleep(1)
+
+    servo1.max()
+    sleep(1)
+
+    step_backward(DEGREES * row)
+    sleep(1)
 
 
-#  mid_servo()
-#  sleep(1)
-#  step_forward(DEGREES)
-#  sleep(1)
-#  max_servo()
-#  sleep(1)
-#  step_backward(DEGREES)
+def servo_shelf(position, open_shelf=False):
+    row, col = position.values()
+    print(f"row: {row}, col: {col}")
+
+    # use row to specify the shelf (servo)
+    selected_servo = select_servo(row)
+
+    # TODO: use col to light a LED
+
+    if open_shelf:
+        selected_servo.max()
+    else:
+        selected_servo.min()
+
+    return True
+
+
+def select_servo(row):
+    return servos[row]
--- servo.py
+++ servo.py
@@ -1,6 +1,5 @@
 from gpiozero.pins.pigpio import PiGPIOFactory
-from gpiozero import Servo, Device, AngularServo
-from time import sleep
+from gpiozero import Device, AngularServo
 
 # Create a PiGPIOFactory object for the local Pi
 factory = PiGPIOFactory()
@@ -8,16 +7,22 @@ factory = PiGPIOFactory()
 # Set the pin factory for gpiozero to use the PiGPIOFactory object
 Device.pin_factory = factory
 
-servo = AngularServo(23, min_angle=-90, max_angle=90)
+# TODO: specify the initial angle of the servo
+servo1 = AngularServo(23, min_angle=-90, max_angle=90)
+servo2 = AngularServo(4, min_angle=-90, max_angle=90)
+
 
 def min_servo():
-    servo.min()
+    servo1.min()
+
 
 def max_servo():
-    servo.max()
+    servo1.max()
+
 
 def mid_servo():
-    servo.mid()
+    servo1.mid()
+
 
 def servo_angle(angle):
-    servo.angle = angle
+    servo1.angle = angle
