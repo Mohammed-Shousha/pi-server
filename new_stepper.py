653ba5dd2ecf43a9008b2196366b80b360b5c82a
Mohammed Shousha <mshousha29@gmail.com>
2023-06-03 16:18:50 +0300

    using new stepper motor

---
+++ new_stepper.py
@@ -0,0 +1,58 @@
+from gpiozero import DigitalOutputDevice
+from time import sleep
+from enum import Enum
+
+STEPS_PER_REV = 200  # at 1:1 microstepping
+
+# Define the pins for the TB6600 driver
+PUL_X = DigitalOutputDevice(17)
+DIR_X = DigitalOutputDevice(27)
+
+PUL_Y = DigitalOutputDevice(23)
+DIR_Y = DigitalOutputDevice(24)
+
+class Direction(Enum):
+    CW = True
+    CCW = False
+
+
+class Axis(Enum):
+    X = "x"
+    Y = "y"
+
+
+steppers = {
+    Axis.X: {"DIR": DIR_X, "PUL": PUL_X},
+    Axis.Y: {"DIR": DIR_Y, "PUL": PUL_Y},
+}
+
+def stepper(axis=Axis.X, steps=STEPS_PER_REV, direction=Direction.CW, delay=0.001):
+
+    DIR, PUL = steppers[axis].values()
+
+    print('dir: ', DIR, 'pul: ', PUL, 'steps: ', steps,
+          'direction: ', direction, 'delay: ', delay, '\n')
+
+    if direction == Direction.CW:
+        DIR.on()
+    else:
+        DIR.off()
+
+    for _ in range(steps):
+        PUL.on()
+        sleep(delay)
+        PUL.off()
+        sleep(delay)
+
+
+def stepper_x_cw(steps):
+    stepper(Axis.X, steps, Direction.CW)
+
+def stepper_x_ccw(steps):
+    stepper(Axis.X, steps, Direction.CCW)
+
+def stepper_y_cw(steps):
+    stepper(Axis.Y, steps, Direction.CW)
+
+def stepper_y_ccw(steps):
+    stepper(Axis.Y, steps, Direction.CCW)
